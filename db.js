e9df3b5c3b176baad1bdfa664a5a750b530bd504
Andrii Kovalenko <andrew.kovalenko4@gmail.com>
2022-04-30 00:49:07 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const dbConfig = require("./db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
+  port: '6432',
   dialect: dbConfig.dialect,
   // declaring pool is optional
   // pool: {
