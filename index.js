aab84f6cbc27ff16d6d7caa991325cf46974d2fd
Andrii Kovalenko <andrew.kovalenko4@gmail.com>
2022-04-29 23:53:57 +0300

    additional changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const telegram = require('node-telegram-bot-api')
 
 // const bot = new telegram(process.env.TELEGRAM_TOKEN, {polling: true})
-
+const UserModel = require('./models');
 const token = '5397416186:AAEJ0k3OV8lBkckoVon1bio127fmwIrpyyY'
 
 const bot = new telegram(token, {polling: true})
