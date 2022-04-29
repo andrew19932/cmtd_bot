8b1b045a909df3c98e5f78b6687f291242c3ceb8
Andrii Kovalenko <andrew.kovalenko4@gmail.com>
2022-04-29 23:39:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,41 @@
 
 const telegram = require('node-telegram-bot-api')
 
-const bot = new telegram(process.env.TELEGRAM_TOKEN, {polling: true})
+// const bot = new telegram(process.env.TELEGRAM_TOKEN, {polling: true})
 
-bot.on('message', msg => {
-   const text = msg.text;
-   const chatId = msg.chat.id;
+const token = '5397416186:AAEJ0k3OV8lBkckoVon1bio127fmwIrpyyY'
 
-   if {text === '/start')
-   bot.sendMessage(chatId, 'добро пожаловать в чат ${text}')
-// const getRandomTip = async () => {
-//   var data = require('./tips.json')
-//   var obj = JSON.parse(JSON.stringify(data))
-//   var randomNumber = Math.floor(Math.random() * 68)
-//   return `Tip ${randomNumber} : ${obj.tips[randomNumber].name}
-// ${obj.tips[randomNumber].text}`;
+const bot = new telegram(token, {polling: true})
+
+bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+
+        try {
+            if (text === '/start') {
+                await UserModel.create({chatId})
+                await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
+                return bot.sendMessage(chatId, `Вітаю в талеграм боті `);
+            }
+            if (text === '/info') {
+                const user = await UserModel.findOne({chatId})
+                return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name});
+            }
+            if (text === '/game') {
+                return startGame(chatId);
+            }
+            return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!)');
+        } catch (e) {
+            return bot.sendMessage(chatId, 'Произошла какая то ошибочка!)');
+        }
+
+    })
 // }
 //
 // const main = async () => {
 //   const randomTip = await getRandomTip();
 //   bot.sendMessage(process.env.TELEGRAM_CHAT_ID, randomTip);
 //   console.log(randomTip);
-}
-
+// }
+start()
 // main();
