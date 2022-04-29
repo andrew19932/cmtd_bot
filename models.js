95abd779e5247c2271f994dc10e5600f9806c4be
Andrii Kovalenko <andrew.kovalenko4@gmail.com>
2022-04-30 00:56:04 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,6 +1,7 @@
 const sequelize = require('./db');
 const {DataTypes} = require('sequelize');
 
+module.exports = (DataTypes , sequelize) => {
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
     chatId: {type: DataTypes.STRING, unique: true},
@@ -8,4 +9,5 @@ const User = sequelize.define('user', {
     wrong: {type: DataTypes.INTEGER, defaultValue: 0},
 })
 
-module.exports = User;
+// module.exports = User;
+};
