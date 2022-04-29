1d80486dc56aad93b70cdd6412f815bdd533083c
Andrii Kovalenko <andrew.kovalenko4@gmail.com>
2022-04-29 23:49:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,30 +8,32 @@ const token = '5397416186:AAEJ0k3OV8lBkckoVon1bio127fmwIrpyyY'
 
 const bot = new telegram(token, {polling: true})
 
-bot.on('message', async msg => {
-        const text = msg.text;
-        const chatId = msg.chat.id;
+const start = async () => {
 
-        try {
-            if (text === '/start') {
-                await UserModel.create({chatId})
-                await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
-                return bot.sendMessage(chatId, `Вітаю в талеграм боті `);
-            }
-            // if (text === '/info') {
-            //     const user = await UserModel.findOne({chatId})
-            //     return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name});
-            // }
-            // if (text === '/game') {
-            //     return startGame(chatId);
-            // }
-            return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!)');
-        } catch (e) {
-            return bot.sendMessage(chatId, 'Произошла какая то ошибочка!)');
-        }
+      bot.on('message', async msg => {
+              const text = msg.text;
+              const chatId = msg.chat.id;
 
-    })
-// }
+              try {
+                  if (text === '/start') {
+                      await UserModel.create({chatId})
+                      await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
+                      return bot.sendMessage(chatId, `Вітаю в талеграм боті `);
+                  }
+                  // if (text === '/info') {
+                  //     const user = await UserModel.findOne({chatId})
+                  //     return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name});
+                  // }
+                  // if (text === '/game') {
+                  //     return startGame(chatId);
+                  // }
+                  return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!)');
+              } catch (e) {
+                  return bot.sendMessage(chatId, 'Произошла какая то ошибочка!)');
+              }
+
+          })
+}
 //
 // const main = async () => {
 //   const randomTip = await getRandomTip();
