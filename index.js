5eb3b3b55ea00960af3299d7e8c4fc15499c62f1
andriihedz <ahedz@conductor.com>
2022-04-30 18:20:27 +0300

    roll back

--- index.js
+++ index.js
@@ -1,63 +1,76 @@
+// require ('dotenv').config()
+
 const TelegramBot = require('node-telegram-bot-api');
 
+const COMMAND_TEMPLATE1 = 'template1';
+const COMMAND_TEMPLATE2 = 'template2';
+const COMMAND_TEMPLATE3 = 'template3';
+const COMMAND_TEMPLATE4 = 'template4';
+const COMMAND_TEMPLATE5 = 'template5';
+let inline_keyboard = [
+    [
+         {
+            text: 'Домашня сторінка',
+            callback_data: COMMAND_TEMPLATE1
+        },
+        {
+            text: 'Акушерсько-гінекологічна домопога',
+            callback_data: COMMAND_TEMPLATE2
+        }
+
+    ],[
+        {
+            text: 'Жіноча консультація',
+            callback_data: COMMAND_TEMPLATE3
+        },
+        {
+            text: 'Терапія',
+            callback_data: COMMAND_TEMPLATE4
+        },
+        {
+            text: 'Контактні дані',
+            callback_data: COMMAND_TEMPLATE5
+        }
+
+    ]
+];
 const options = {
     polling: true
 };
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, options);
 
-const HINECOLOGICAL_HELP = '/hinecology_help';
-const GINECOLOGY_HELP = '/ginecology_help';
-const WOMEN_CONSULTATION = '/women_consultation';
-const THERAPY = '/therapy';
-const CONTACTS = '/contacts';
 
 bot.setMyCommands([
-    {command: '/start', description: 'Домашня сторінка'},
-    {command: GINECOLOGY_HELP, description: 'Акушерсько-гінекологічна домопога'},
-    {command: WOMEN_CONSULTATION, description: 'Жіноча консультація'},
-    {command: THERAPY, description: 'Терапія'},
-    {command: CONTACTS, description: 'Контактні дані'},
+  {command: '/start', description: 'Домашня сторінка'},
+  {command: '/ginecology_help', description: 'Акушерсько-гінекологічна домопога'},
+  {command: '/women_consultation', description: 'Жіноча консультація'},
+  {command: '/therapy', description: 'Терапія'},
+  {command: '/contacts', description: 'Контактні дані'},
 ])
 
 function process_message(text, chatId) {
-    try {
-        if (text === '/start') {
-            bot.sendMessage(chatId, "Вітаємо в телеграм-боті КПМ ВМКЛ ЦМ та Д.\nВ розділі меню оберіть пункт, який Вас цікавить:", {
-                "reply_markup": {
-                    "inline_keyboard": [
-                        [
-                            {
-                                text: 'Акушерсько-гінекологічна домопога',
-                                callback_data: GINECOLOGY_HELP
-                            },
-                            {
-                                text: 'Жіноча консультація',
-                                callback_data: WOMEN_CONSULTATION
-                            }
-                        ],
-                        [
-                            {
-                                text: 'Терапія',
-                                callback_data: THERAPY
-                            },
-                            {
-                                text: 'Контактні дані',
-                                callback_data: CONTACTS
-                            }
-                        ]
-                    ]
-                }
-            });
-        } else if (text === HINECOLOGICAL_HELP) {
-            bot.sendMessage(chatId, `Акушерсько-гінекологічна домопога.\nЛікарі центру проводять комплексну роботу з кожним пацієнтом, що включає консультацію, діагностику на сучасному обладнанні. Це дозволяє підібрати підходи для ефективного лікування.\nКонтактна інформація - mlcentr@ukr.net`);
-        } else if (text === GINECOLOGY_HELP) {
-            bot.sendMessage(chatId, `Акушерсько-гінекологічна домопога.\nЛікарі центру проводять комплексну роботу з кожним пацієнтом, що включає консультацію, діагностику на сучасному обладнанні. Це дозволяє підібрати підходи для ефективного лікування.\nКонтактна інформація - mlcentr@ukr.net`);
-        } else if (text === THERAPY) {
-            bot.sendMessage(chatId, `Секція терапії.\nТерапевт Вівчарик Налатія Василівна.\nКонтактна інформація - @VivcharykNV`);
-        } else if (text === WOMEN_CONSULTATION) {
-            bot.sendMessage(chatId, `Секція жіночої консультації.\nУ нас працюють кваліфіковані акушери-гінекологи, яким Ви можете довіритися. Лікарі з дуже великим стажем роботи, що пройшли тренінги, які володіють усіма сучасними методами прийому пологів\nКонтактна інформація - @VivcharykNV`);
-        } else if (text === CONTACTS) {
-            bot.sendMessage(chatId, ` Директор	Присяжнюк Володимир Петрович	65 11 12 \n
+  try {
+      if (text === '/start') {
+          return bot.sendMessage(chatId, "Вітаємо в телеграм-боті КПМ ВМКЛ ЦМ та Д.\nВ розділі меню оберіть пункт, який Вас цікавить:", {
+              "reply_markup": {
+                  "inline_keyboard": inline_keyboard
+              }
+          });
+      }
+      if (text === '/hinecology_help') {
+          return bot.sendMessage(chatId, `Акушерсько-гінекологічна домопога.\nЛікарі центру проводять комплексну роботу з кожним пацієнтом, що включає консультацію, діагностику на сучасному обладнанні. Це дозволяє підібрати підходи для ефективного лікування.\nКонтактна інформація - mlcentr@ukr.net`);
+      }
+      if (text === '/ginecology_help') {
+          return bot.sendMessage(chatId, `Акушерсько-гінекологічна домопога.\nЛікарі центру проводять комплексну роботу з кожним пацієнтом, що включає консультацію, діагностику на сучасному обладнанні. Це дозволяє підібрати підходи для ефективного лікування.\nКонтактна інформація - mlcentr@ukr.net`);
+      }
+      if (text === '/therapy') {
+          return bot.sendMessage(chatId, `Секція терапії.\nТерапевт Вівчарик Налатія Василівна.\nКонтактна інформація - @VivcharykNV`);
+      }
+      if (text === '/women_consultation') {
+          return bot.sendMessage(chatId, `Секція жіночої консультації.\nУ нас працюють кваліфіковані акушери-гінекологи, яким Ви можете довіритися. Лікарі з дуже великим стажем роботи, що пройшли тренінги, які володіють усіма сучасними методами прийому пологів\nКонтактна інформація - @VivcharykNV`);
+      }
+      if (text === '/contacts') {
+          return bot.sendMessage(chatId, ` Директор	Присяжнюк Володимир Петрович	65 11 12 \n
                                            \n Медичний директор	Бардаш Леся Юріївна	65 11 22 \n
                                            \n Заступник медичного директора з педіатрії	Опіопченко Світлана Федорівна	65 11 12 \n
                                            \n Заступник директора з економічних питань	Дунаєва Ірина Дмитрівна	65 11 20\n
@@ -65,38 +78,50 @@ function process_message(text, chatId) {
                                            \nГоловна медична сестра	Коваль Ірина Анатоліївна	65 11 36\n
                                            \nГоловний бухгалтер	Костюк Людмила Володимирівна	65 11 19\n
                                            \nНачальник відділу кадрів	Ключківська Тетяна Володимирівна	65 11 36\n`);
-        } else {
-            bot.sendMessage(chatId, 'Не зрозуміла команда спробуй ще раз!)');
-        }
-    } catch (e) {
-        bot.sendMessage(chatId, 'Виникла помилка!)');
-    }
+      }
+      return bot.sendMessage(chatId, 'Не зрозуміла команда спробуй ще раз!)');
+  } catch (e) {
+      return bot.sendMessage(chatId, 'Виникла помилка!)');
+  }
 }
 
-bot.onText(/\/start/, function onEditableText(msg) {
+bot.on('message', async msg => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
 
-    process_message('/start', msg.from.id);
-});
-bot.onText(/\/ginecology_help/, function onEditableText(msg) {
+  return process_message(text, chatId);
 
-    process_message(GINECOLOGY_HELP, msg.from.id);
-});
-bot.onText(/\/women_consultation/, function onEditableText(msg) {
-
-    process_message(WOMEN_CONSULTATION, msg.from.id);
-});
-bot.onText(/\/therapy/, function onEditableText(msg) {
-
-    process_message(THERAPY, msg.from.id);
-});
-bot.onText(/\/contacts/, function onEditableText(msg) {
-
-    process_message(CONTACTS, msg.from.id);
 });
 
-// Handle callback queries
 bot.on('callback_query', function onCallbackQuery(callbackQuery) {
-    const action = callbackQuery.data;
-    const msg = callbackQuery.message;
-    process_message(action, msg.chat.id);
+  const action = callbackQuery.data;
+  const msg = callbackQuery.message;
+  const opts = {
+    chat_id: msg.chat.id,
+    message_id: msg.message_id,
+  };
+  let text;
+
+  if (action === COMMAND_TEMPLATE1) {
+    return bot.sendMessage(msg.chat.id, `Секція жіночої консультації.\nУ нас працюють кваліфіковані акушери-гінекологи, яким Ви можете довіритися. Лікарі з дуже великим стажем роботи, що пройшли тренінги, які володіють усіма сучасними методами прийому пологів\nКонтактна інформація - @VivcharykNV`);
+  }
+  if (action === COMMAND_TEMPLATE2) {
+    return bot.sendMessage(msg.chat.id, `Акушерсько-гінекологічна домопога.\nЛікарі центру проводять комплексну роботу з кожним пацієнтом, що включає консультацію, діагностику на сучасному обладнанні. Це дозволяє підібрати підходи для ефективного лікування.\nКонтактна інформація - mlcentr@ukr.net`);
+  }
+  if (action === COMMAND_TEMPLATE3) {
+    return bot.sendMessage(msg.chat.id, `Секція жіночої консультації.\nУ нас працюють кваліфіковані акушери-гінекологи, яким Ви можете довіритися. Лікарі з дуже великим стажем роботи, що пройшли тренінги, які володіють усіма сучасними методами прийому пологів\nКонтактна інформація - @VivcharykNV`);
+  }
+  if (action === COMMAND_TEMPLATE4) {
+    return bot.sendMessage(msg.chat.id, `Секція терапії.\nТерапевт Вівчарик Налатія Василівна.\nКонтактна інформація - @VivcharykNV`);
+  }
+  if (action === COMMAND_TEMPLATE5) {
+    return bot.sendMessage(msg.chat.id, ` Директор	Присяжнюк Володимир Петрович	65 11 12 \n
+                                     \n Медичний директор	Бардаш Леся Юріївна	65 11 22 \n
+                                     \n Заступник медичного директора з педіатрії	Опіопченко Світлана Федорівна	65 11 12 \n
+                                     \n Заступник директора з економічних питань	Дунаєва Ірина Дмитрівна	65 11 20\n
+                                     \nЗаступник директора з технічних питань	Руденко Марина Олександрівна	65 11 23\n
+                                     \nГоловна медична сестра	Коваль Ірина Анатоліївна	65 11 36\n
+                                     \nГоловний бухгалтер	Костюк Людмила Володимирівна	65 11 19\n
+                                     \nНачальник відділу кадрів	Ключківська Тетяна Володимирівна	65 11 36\n`);
+  }
 });
