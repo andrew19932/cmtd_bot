79f9acff5153463c814c68d4097b40d62bf24847
Andrii Kovalenko <andrew.kovalenko4@gmail.com>
2022-04-30 15:07:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
   if (action === COMMAND_TEMPLATE2) {
     text === 'Секція жіночої консультації.\nУ нас працюють кваліфіковані акушери-гінекологи, яким Ви можете довіритися. Лікарі з дуже великим стажем роботи, що пройшли тренінги, які володіють усіма сучасними методами прийому пологів\nКонтактна інформація - @VivcharykNV`';
   }
-  bot.sendMessage(chatId, text);
+  // bot.sendMessage(chatId, text);
 // bot.on('callback_query', function onCallbackQuery(callbackQuery) {
 //     const msg = callbackQuery.message;
 //     let text;
@@ -142,5 +142,5 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
 //     }
 
 
-  // process_message(text, msg.chat.id);
+  process_message(text, msg.chat.id);
 });
